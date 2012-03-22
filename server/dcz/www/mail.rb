b4261dba0ae23e3a3378918d7e323cf443daebdd
Pierre Chapuis <catwell@archlinux.us>
2012-03-22 20:21:24 +0100

    fix

--- server/dcz/www/mail.rb
+++ server/dcz/www/mail.rb
@@ -1,7 +1,7 @@
 module Dcz class Www
 
   get("/confirm-vote/:x/?") do |x|
-    opinion = Vote.get_by_confirmation_id(x)
+    opinion = Opinion.get_by_confirmation_id(x)
     opinion.confirm!
     "done"
   end
